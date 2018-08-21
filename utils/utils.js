c63aa3028de4c2b5d46a61c7cb71ef093fb3fd79
xerxes23 <josephmendoza23@gmail.com>
2018-08-21 15:06:18 -0400

    Set up expect assertion lib and modify tests

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,10 @@
 module.exports.add = (a, b) => a + b;
 
 module.exports.square = a => a * a;
+
+module.exports.setName = (user, fullName) => {
+  const name = fullName.split(" ");
+  user.firstName = name[0];
+  user.lastName = name[1];
+  return user;
+};
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,27 @@
+const expect = require("expect");
+
 const utils = require("./utils");
 
 it("should add to two numbers", () => {
   const res = utils.add(20, 3);
 
-  if (res !== 23) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res)
+    .toBe(23)
+    .toBeA("number");
 });
 
 it("should square a number", () => {
   const res = utils.square(4);
+  expect(res).toBe(16);
+});
 
-  if (res !== 16) {
-    throw new Error(`Expected 16, but got ${res}`);
-  }
+// it('should expect some values', () => {
+//   expect(12).toBe(12);
+//   expect({name: 'Joseph'}).toNotEqual({name: 'Jose'});
+// })
+
+it("should set first and last names", () => {
+  const user = utils.setName({}, "Joseph Mendoza");
+
+  expect(user).toInclude({ firstName: "Joseph", lastName: "Mendoza" });
 });
