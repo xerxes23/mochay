a90d4ce67f1c4e1fc998cf5e090ab5acb808014b
xerxes23 <josephmendoza23@gmail.com>
2018-08-22 11:02:41 -0400

    Group tests into describe blocks

--- server/server.test.js
+++ server/server.test.js
@@ -1,23 +1,29 @@
-const request = require("supertest");
 const expect = require("expect");
 const app = require("./server");
+const request = require("supertest");
 
-it("should return error response", done => {
-  request(app)
-    .get("/")
-    .expect(404)
-    .expect(res => {
-      expect(res.body).toInclude({ error: "Page not found" });
-    })
-    .end(done);
-});
+describe("Server", () => {
+  describe("GET /", () => {
+    it("should return error response", done => {
+      request(app)
+        .get("/")
+        .expect(404)
+        .expect(res => {
+          expect(res.body).toInclude({ error: "Page not found" });
+        })
+        .end(done);
+    });
+  });
 
-it("should return users array", done => {
-  request(app)
-    .get("/users")
-    .expect(200)
-    .expect(res => {
-      expect(res.body).toInclude({ name: "Joe Mendoza", role: "CEO" });
-    })
-    .end(done);
+  describe("GET /users", () => {
+    it("should return users array", done => {
+      request(app)
+        .get("/users")
+        .expect(200)
+        .expect(res => {
+          expect(res.body).toInclude({ name: "Joe Mendoza", role: "CEO" });
+        })
+        .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,43 +2,45 @@ const expect = require("expect");
 
 const utils = require("./utils");
 
-it("should add to two numbers", () => {
-  const res = utils.add(20, 3);
+describe("Utils", () => {
+  it("should add to two numbers", () => {
+    const res = utils.add(20, 3);
 
-  expect(res)
-    .toBe(23)
-    .toBeA("number");
-});
-
-it("should add two numbers asynchronously", done => {
-  utils.asyncAdd(7, 3, sum => {
-    expect(sum)
-      .toBe(10)
-      .toBeA("number");
-    done();
-  });
-});
-
-it("should square a number", () => {
-  const res = utils.square(4);
-  expect(res).toBe(16);
-});
-
-it("should square a number asynchronously", done => {
-  utils.asyncSquare(3, res => {
     expect(res)
-      .toBe(9)
+      .toBe(23)
       .toBeA("number");
-    done();
   });
-});
-// it('should expect some values', () => {
-//   expect(12).toBe(12);
-//   expect({name: 'Joseph'}).toNotEqual({name: 'Jose'});
-// })
 
-it("should set first and last names", () => {
-  const user = utils.setName({}, "Joseph Mendoza");
+  it("should add two numbers asynchronously", done => {
+    utils.asyncAdd(7, 3, sum => {
+      expect(sum)
+        .toBe(10)
+        .toBeA("number");
+      done();
+    });
+  });
 
-  expect(user).toInclude({ firstName: "Joseph", lastName: "Mendoza" });
+  it("should square a number", () => {
+    const res = utils.square(4);
+    expect(res).toBe(16);
+  });
+
+  it("should square a number asynchronously", done => {
+    utils.asyncSquare(3, res => {
+      expect(res)
+        .toBe(9)
+        .toBeA("number");
+      done();
+    });
+  });
+  // it('should expect some values', () => {
+  //   expect(12).toBe(12);
+  //   expect({name: 'Joseph'}).toNotEqual({name: 'Jose'});
+  // })
+
+  it("should set first and last names", () => {
+    const user = utils.setName({}, "Joseph Mendoza");
+
+    expect(user).toInclude({ firstName: "Joseph", lastName: "Mendoza" });
+  });
 });
